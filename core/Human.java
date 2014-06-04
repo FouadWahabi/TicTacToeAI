0b572713aa14664ac1581f97431668375c87de5f
fouad662 <p0won.xhacker@gmail.com>
2014-06-04 19:11:31 +0200

    Create Human.java

---
+++ core/Human.java
@@ -0,0 +1,21 @@
+package core;
+
+import java.awt.Point;
+import java.util.Scanner;
+
+public class Human {
+
+	public boolean firstPlayer;
+	public char marker;
+	public void playMove(Board board) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		Point p;
+		do {
+		System.out.println("It's your turn");
+		p = new Point(in.nextInt(), in.nextInt());
+		} while(board.contain(p, board.moves));
+		board.moves.add(new Move(marker,p));
+	}
+
+}
