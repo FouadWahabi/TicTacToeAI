104d531518731fe75913a4d33eaa124d0a4ca962
fouad662 <p0won.xhacker@gmail.com>
2014-06-04 19:30:11 +0200

    Update Human.java

--- core/Human.java
+++ core/Human.java
@@ -12,7 +12,7 @@ public class Human {
 		Scanner in = new Scanner(System.in);
 		Point p;
 		do {
-		System.out.println("It's your turn");
+		System.out.println("It's your turn enter coordinates like thi : 0 0");
 		p = new Point(in.nextInt(), in.nextInt());
 		} while(board.contain(p, board.moves));
 		board.moves.add(new Move(marker,p));
